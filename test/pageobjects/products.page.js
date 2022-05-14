bef9a6f79c8df5b746d3035a873138eb79c0281d
Vitalina Boiko <vita@vita.vita>
2022-05-14 12:14:54 +0200

    last changes

--- test/pageobjects/products.page.js
+++ test/pageobjects/products.page.js
@@ -1,13 +1,24 @@
-class ProductsPage {
+const  Page = require('.//inventory');
 
-    get productsCount() {
-        return 5;
+class ProductsPage extends Page{
+
+    // get productsCount() {
+    //     return 5;
+    // }
+
+    // async AddToCartFirstItem() {
+    //     const inventoryList = await $('.inventory_list');
+    //     await inventoryList.$$('button')[0].click();
+    // }
+
+    async FetchAllProductsInList() {
+        const addToCartProducts = await $$('.btn_inventory')
+        await addToCartProducts.forEach(async (addToCartProduct) => {
+            await addToCartProduct.click()
+        })
     }
 
-    async AddToCartFirstItem() {
-        const inventoryList = await $('.inventory_list');
-        await inventoryList.$$('button')[0].click();
-    }
+
 }
 
 module.exports = new ProductsPage();
--- test/specs/e2e-goods-order.js
+++ test/specs/e2e-goods-order.js
@@ -8,8 +8,10 @@ describe('E2E goods order', () => {
         LoginPage.open();
         await LoginPage.login('standard_user', 'secret_sauce');
 
-        await ProductsPage.AddToCartFirstItem();
-        // await ProductsPage.GoToCart();
+       // await ProductsPage.AddToCartFirstItem();
+        
+       await ProductsPage.FetchAllProductsInList();
+       // await ProductsPage.GoToCart();
 
     });
 
